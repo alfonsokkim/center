746522d7423c24ea7b4b9428eec7ec21731c5a5e
iGunB <iangunhee@gmail.com>
2026-03-14 15:43:35 +1100

    session routing template

--- backend/routes/session.py
+++ backend/routes/session.py
@@ -1,2 +1,23 @@
 from fastapi import APIRouter;
 router = APIRouter()
+
+@router.post("/start")
+async def tmp1():
+    return None
+
+@router.patch("/pause")
+async def tmp2():
+    return None
+
+@router.patch("/resume")
+async def tmp3():
+    return None
+
+@router.delete("/end")
+async def tmp4():
+    return None
+
+@router.get("/timer")
+async def tmp5():
+    return None
+
